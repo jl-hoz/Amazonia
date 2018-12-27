5c6c862c449d16228b9ad54637a9a023f7c2aa8a
José Luis de la Hoz <joseluisdelahozgarcia@gmail.com>
2018-12-27 21:59:04 +0100

    Time class (Thread)

---
+++ src/main/java/model/Time.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.Random;
+
+import javafx.scene.control.Button;
+
+public class Time extends Thread{
+	
+	@Override
+	public void run() {
+		try {
+			sleep(generateRandomTime());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 
+	 * @return random int with 0 to 3000 range.
+	 */
+	private int generateRandomTime() {
+		Random x = new Random();
+		return x.nextInt(3000) + 1;
+	}
+	
+}
